e88bf33f794e50d89eba940c01341bc278177fa5
Nahuel Tello <67810116+NahuelTello@users.noreply.github.com>
2023-05-02 23:14:41 -0300

    AGREGANDO DOM EN LA BUSQUEDA DE CLIENTES

--- main.js
+++ main.js
@@ -1,5 +1,75 @@
+/* Quiero armar una pagina donde me muestre una lista de clientes
+donde pueda buscar por su nombre (filtro), tambien pueda agregar y
+modificar el nombre */
+const formulario = document.getElementById("formulario")
+const main = document.getElementById("contenedor")
+const buscadorMain = document.getElementById("buscador")
+const buscadorCliente = document.getElementById("buscador-cliente")
 
 // creamos la funcion de objeto persona
+class Cliente {
+    constructor (nombre, apellido, sexo, edad) {
+        this.nombre = nombre,
+            this.apellido = apellido,
+            this.sexo = sexo,
+            this.edad = edad
+    }
+}
+
+let cliente1 = new Cliente("Juan","Pérez","masculino",30)
+let cliente2 = new Cliente("Maria","Gomez","femenino",25)
+let cliente3 = new Cliente("Carlos","Hernandez","masculino",45)
+let cliente4 = new Cliente("Josefina", "Hernandez", "femenino", 15)
+let cliente5 = new Cliente("Matias", "Martinez", "masculino", 35)
+
+
+const listaClientes = [cliente1, cliente2, cliente3, cliente4, cliente5];
+
+
+/* //Iteramos el array con for...of
+for (const cliente of listaClientes) {
+    //Creamos un nodo <ol> y agregamos al padre en cada ciclo
+    let li = document.createElement("ol");
+    li.innerHTML = `<h1> ${cliente.nombre} </h1>
+                    <h3> ${cliente.apellido} </h3>
+                    <p> ${cliente.sexo} </p>
+                    <p> ${cliente.edad} </p>
+                    <hr></hr>`
+    main.appendChild(li);
+}
+console.table(listaClientes) */
+
+
+//Definimos una funcion que pide el nombre de la persona para saber si se encuentra
+function buscarPersona() {
+    let clienteBuscado = buscadorCliente.value.trim().toUpperCase()
+    
+
+    //Utilizamos la funcion de orden superior para filtrar el elemento persona
+    let clienteEncontrado = listaClientes.filter((cliente) => cliente.nombre.toUpperCase().includes(clienteBuscado))
+
+    //Si esta dentro del rango muestra la persona por consola
+    if (clienteEncontrado.length > 0) {
+        for (const cliente of listaClientes) {
+            //Creamos un nodo <ol> y agregamos al padre en cada ciclo
+            let mostrarCliente = document.createElement("ol");
+            mostrarCliente.innerHTML = `<h1> ${cliente.nombre} </h1>
+                    <h3> ${cliente.apellido} </h3>
+                    <p> ${cliente.sexo} </p>
+                    <p> ${cliente.edad} </p>
+                    <hr></hr>`
+            main.appendChild(mostrarCliente);
+        }
+        //console.table(clienteEncontrado)
+    } else {
+        //En caso contrario no se pudo encontrar
+        let eventError = document.createElement("p")
+        eventError.innerHTML = `<p>No se pudo encontrar ninguna persona con ese nombre</p>`
+        main.append(eventError)
+    }
+}
+
+/* // creamos la funcion de objeto persona
 class Persona { 
     constructor (nombre, apellido, sexo, edad){
         this.nombre = nombre,
@@ -95,7 +165,10 @@ while (opcion != 'x' && opcion != 'X') {
     }
 
     opcion = prompt('Ingrese una opcion. \n1) Buscar Cliente. \n2) Agregar Cliente. \n3) Modificar Cliente \n Salir (X) ')
-}
+} */
+
+
+
 
 
 
